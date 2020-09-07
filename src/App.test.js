decea9eb50d2df7e8e6d40c9904bb1cf60198da7
“brunoreyes” <bruno619reyes@gmail.com>
2020-09-07 12:05:04 -0500

    added modification to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from 'components/App';
+import App from 'src/components/App/App.js';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
