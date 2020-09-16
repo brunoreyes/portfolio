3219420ed67f322a4cfaeca59af9e4d91650797f
brunoreyes <bruno619reyes@gmail.com>
2020-09-16 11:50:28 -0500

    Updates

---
+++ precache-manifest.b9e41696141920d3b28b77f60a97011a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aa8b3b88382f64239c46f027150d55",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "56586b7a2a209bd99710",
+    "url": "/my-app/static/css/main.a108e7ab.chunk.css"
+  },
+  {
+    "revision": "e0c6c87f6e8828d04bdd",
+    "url": "/my-app/static/js/2.b48ba00a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.b48ba00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56586b7a2a209bd99710",
+    "url": "/my-app/static/js/main.288625f4.chunk.js"
+  },
+  {
+    "revision": "999e1c7becdf877982e5",
+    "url": "/my-app/static/js/runtime-main.d01f81af.js"
+  }
+]);
