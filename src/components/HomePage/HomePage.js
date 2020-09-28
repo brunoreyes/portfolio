7950d4c14f4dc32d07d57b1690b70e40937c26a1
brunoreyes <bruno619reyes@gmail.com>
2020-09-28 01:36:54 -0500

    turned off project view mode

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -64,7 +64,7 @@ class HomePage extends Component {
     darkMode: false,
     messageMode: false,
     sentMode: false,
-    projectMode: true,
+    projectMode: false,
   };
 
   toggleDarkMode = () => {
